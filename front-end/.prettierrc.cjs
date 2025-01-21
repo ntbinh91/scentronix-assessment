4d5c2841ede1ae94cb50bea035105e2a05b3c6b0
Binh Nguyen <ntbinh91@gmail.com>
2025-01-21 15:18:12 +0700

    Install prettier for front-end folder

---
+++ front-end/.prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+};
